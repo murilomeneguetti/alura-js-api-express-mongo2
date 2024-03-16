18ecc548146c0d8c01237960b7566d020e66b764
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2024-03-16 11:08:26 -0300

    dotenv

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose"
 
+//variaveis de ambiente
+//ao inves de usar o link aqui, poderia usar o dotenv: process.env.STRING_CONEXAO_DB
+
 mongoose.connect("mongodb+srv://admin:admin123@cluster0.fp9ov5b.mongodb.net/livraria?retryWrites=true&w=majority");
 
 let db = mongoose.connection;
