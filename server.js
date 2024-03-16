719a900435fa247434f130e06e71bf7a4b093c21
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2024-03-16 09:07:22 -0300

    apagando versão antiga

--- server.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import app from "./src/app.js";
-
-//porta 3000 onde a conexão vai acontecer
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log('servidor escutando!');
-})
--- src/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-async function conectaNaDatabase() {
-    //método do mongoose para conectar com o banco mongodb
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
-    return mongoose.connection;
-};
-
-export default conectaNaDatabase;
